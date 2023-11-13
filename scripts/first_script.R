2f17edc277d5aeebe70ba0f3c8541c202e46441b
Koen Stork <k.p.stork@student.rug.nl>
2023-11-13 12:05:34 +0100

    project initialized with renv

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,1 +1,15 @@
 #my first github synced script
+library(renv)
+
+#create a lockfile in R project to store current versions of used packages
+renv::init()
+
+# restore the versions of the packages used to develop this project
+renv::restore()
+
+x <- c(1,2,3,4,5)
+y <- c(1,4,3,6,9)
+data <- data.frame(x,y)
+data
+
+
