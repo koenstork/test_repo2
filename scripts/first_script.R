e0624d746b3bf3fcc023a44189a71352f517b88d
Koen Stork <k.p.stork@student.rug.nl>
2023-11-13 12:21:03 +0100

    fig01 produced

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,15 +1,30 @@
-#my first github synced script
+# my first github synced script
 library(renv)
 
-#create a lockfile in R project to store current versions of used packages
+# create a lockfile in R project to store current versions of used packages
 renv::init()
 
 # restore the versions of the packages used to develop this project
 renv::restore()
+# remember to use pull in Git tab to get changes from github
+
+# load libraries
+library(tidyverse)
+
 
 x <- c(1,2,3,4,5)
 y <- c(1,4,3,6,9)
 data <- data.frame(x,y)
 data
+
+fig01 <- data %>% ggplot(aes(x=x, y=y))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+fig01
+ggsave("./figures/fig01.png", plot=fig01, width = 1920, height = 1068, units = "px")
+
+
+
+
 
 
